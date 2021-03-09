664f9fe0168ae3c1069e4376386ef8d8451142f9
AarhonJoshuaa <aarhonjoshuaa001@gmail.com>
2021-03-09 15:23:40 +0530

    Completed

--- settings/config.py
+++ settings/config.py
@@ -1,10 +1,10 @@
 import os
 ENV = "PROD" 
 if ENV == "DEV":
- from settings import keys
- telegram_key = keys.telegram_key
+    from settings import keys
+    telegram_key = keys.telegram_key
 elif ENV == "PROD":
- import ast
- telegram_key = ast.literal_eval(os.environ["telegram_key"])
+    import ast
+    telegram_key = ast.literal_eval(os.environ["telegram_key"])
 host = "0.0.0.0"
 port = int(os.environ.get("PORT", 5000))
